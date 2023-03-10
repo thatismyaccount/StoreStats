6d41f515038fe15b4ec6e04fc6c7ebd5062d1671
Mateusz Brys <mat_bry@o2.pl>
2023-03-10 21:43:56 +0100

    Required Name and remove duplicated model

---
+++ StoreStats.Data/Migrations/202303102042118_ContextUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace StoreStatsApi.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ContextUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Stores", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Stores", "Name", c => c.String());
+        }
+    }
+}
