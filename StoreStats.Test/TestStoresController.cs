3b6a39e992b23258c5206694cc718bbffe026b6e
Mateusz Brys <mat_bry@o2.pl>
2023-03-10 22:34:42 +0100

    Added tests for other methods on StoresController

--- StoreStats.Test/TestStoresController.cs
+++ StoreStats.Test/TestStoresController.cs
@@ -5,6 +5,7 @@ using StoreStats.Data.Models;
 using StoreStatsApi.Test.Contexts;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http.Results;
@@ -33,11 +34,35 @@ namespace StoreStatsApi.Test
             Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
 
+
+        [TestMethod]
+        public async Task DeleteStore_ShouldReturnOk()
+        {
+            var controller = new StoresController(GetTestDbContext());
+
+            var result = await controller.DeleteStore(4) as OkNegotiatedContentResult<DeleteStoreResponse>;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<DeleteStoreResponse>));
+            Assert.AreEqual(true, result.Content.Status);
+        }
+
+        [TestMethod]
+        public async Task DeleteStore_ShouldReturnNotFound()
+        {
+            var controller = new StoresController(GetTestDbContext());
+
+            var result = await controller.DeleteStore(99) as NotFoundResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
         [TestMethod]
         public async Task PutStore_ShouldReturnStatusCode()
         {
             var controller = new StoresController(GetTestDbContext());
-            var item = GetTestStore();
+            var item = GetPutStoreDto();
 
             var result = await controller.PutStore(4, item) as OkNegotiatedContentResult<PutStoreResponse>;
 
@@ -47,10 +72,10 @@ namespace StoreStatsApi.Test
         }
 
         [TestMethod]
-        public async Task PutStore_ShouldReturnBadRequest()
+        public async Task PutStore_DifferentIds_ShouldReturnBadRequest()
         {
             var controller = new StoresController(GetTestDbContext());
-            var item = GetTestStore();
+            var item = GetPutStoreDto();
 
             var result = await controller.PutStore(5, item) as BadRequestResult;
 
@@ -58,9 +83,82 @@ namespace StoreStatsApi.Test
             Assert.IsInstanceOfType(result, typeof(BadRequestResult));
         }
 
-        private PutStoreDto GetTestStore()
+        [TestMethod]
+        public async Task PutStoreDto_ValidationFailed_ShouldReturnBadRequest()
+        {
+            var controller = new StoresController(GetTestDbContext());
+            var item = GetPutStoreDto();
+            controller.ModelState.AddModelError("test", "test");
+
+
+            var result = await controller.PutStore(4, item) as InvalidModelStateResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InvalidModelStateResult));
+        }
+
+        [TestMethod]
+        public void PutStore_NoName_ValidationFailes()
+        {
+            var sut = GetPutStoreDto();
+            sut.Name = null;
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            Assert.IsFalse(isModelStateValid);
+        }
+
+        [TestMethod]
+        public async Task PostStore_ShouldReturnCreatedAt()
+        {
+            var controller = new StoresController(GetTestDbContext());
+            CreateStoreDto item = GetCreateStoreDto();
+            item.Name = null;
+
+            var result = await controller.PostStore(item) as CreatedAtRouteNegotiatedContentResult<PostStoreResponse>;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(CreatedAtRouteNegotiatedContentResult<PostStoreResponse>));
+            
+        }
+
+        [TestMethod]
+        public void CreateStoreDto_NoName_ValidationFails()
+        {
+            var sut = GetCreateStoreDto();
+            sut.Name = null;
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            Assert.IsFalse(isModelStateValid);
+        }
+
+        [TestMethod]
+        public async Task PostStore_ValidationFails_ShouldReturnBadRequest()
+        {
+            var controller = new StoresController(GetTestDbContext());
+            CreateStoreDto item = GetCreateStoreDto();
+
+            controller.ModelState.AddModelError("test", "test");
+
+            var result = await controller.PostStore(item) as InvalidModelStateResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InvalidModelStateResult));
+        }
+
+        private PutStoreDto GetPutStoreDto()
         {
             return new PutStoreDto { Id = 4, Name = "Store #4", City = "Toruń", Country = "Poland" };
+        }
+
+        private static CreateStoreDto GetCreateStoreDto()
+        {
+            return new CreateStoreDto() { City = "Katowice", Country = "Poland", Name = "Store #5" };
         }
 
         private static TestStoreStatsDbContext GetTestDbContext()
