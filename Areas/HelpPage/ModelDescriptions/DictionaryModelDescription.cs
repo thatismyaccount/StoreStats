395cdb94fc5c91af96a38a3377fbe055477ef967
Mateusz Brys <mat_bry@o2.pl>
2023-03-13 00:56:14 +0100

    Clean repo from incorectly added files

--- Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StoreStats.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreStats.WebApiApplication" Language="C#" %>
